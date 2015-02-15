da659b3a48cf4dba88274f92211a461351552832
Olle E. Johansson <oej@edvina.net>
2015-02-15 09:56:36 +0100

    Adding updated create cert command

--- bin/createcert.sh
+++ bin/createcert.sh
@@ -1,26 +1,60 @@
 #!/bin/sh
+ERROR=1
+
+function help()
+{
+	echo "Syntax: <certtype> hostname altname"
+	echo "Certtype:"
+	echo "    md5       MD5-signed certificate"
+	echo "    nosan     Certificate with only CN"
+	echo "    evil      Certificate signed by evil CA"
+	exit
+}
+
 if test -z $1;then
-	COMMONNAME=sip.sipguru.no
-	ALTNAME=sipguru.no
-else
-	if test -z $2;then
-		COMMONNAME=$1
-		ALTNAME=DNS:$1
-	else
-		COMMONNAME=$1
-		ALTNAME=DNS:$2
-	fi
+	help
 fi
-echo Name: COMMONNAME
+if test $1 = md5; then
+	OPTION="$OPTION -digest md5"
+	ERROR=0
+fi
+if test $1 = nosan; then
+	OPTION="$OPTION -extensions usr_cert_no_san"
+	ERROR=0
+fi
+if test $1 = evil; then
+	echo "Mohahahaha 👿 "
+	ERROR=0
+fi
+if test $ERROR = 1;then
+	echo "ERROR - bad command $1. "
+	help
+	exit 1
+fi
+if test -z $3;then
+	# Should possible change "www.skrep.com" and add "skrep.com" as alt name.
+	COMMONNAME=$2
+	ALTNAME=DNS:$2
+else
+	COMMONNAME=$2
+	ALTNAME=DNS:$3
+fi
 #Generate request
+echo "Common Name (CN) $COMMONNAME Alt $ALTNAME"
 export COMMONNAME ALTNAME
+# -digest sha256|md5|sha1
+#	-extensions section
+#	-reqexts section
+#	- utf8		Fields are default ASCII
+#	- verbose  Extra information
+
 openssl req -new -nodes \
 	-out ca/request/$COMMONNAME.req \
 	-keyout ca/private/$COMMONNAME.key \
 	-config etc/openssl.cnf
 
 #Sign request And create cert
-openssl ca  \
+openssl ca  $OPTION \
 	-config etc/openssl.cnf \
 	-out ca/certs/$COMMONNAME.cert \
 	-infiles ca/request/$COMMONNAME.req
